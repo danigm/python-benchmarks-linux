8b64f22b02d1f669ae1b0315746ce37af5612974
Daniel Garcia Moreno <daniel.garcia@suse.com>
2023-07-12 17:17:55 +0200

    Add initial comparison

---
+++ performance-comparison/compare.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+pyperf compare_to --table --table-format md bci154-3.10.json bci155-3.11.json > bci-table.txt
+pyperf compare_to --table --table-format md tw-3.10.json tw-3.11.json > tw-table.txt
+pyperf compare_to --table --table-format md rocky9-3.11.json bci155-3.11.json > rocky-table.txt
+pyperf compare_to --table --table-format md ubuntu2204-3.11.json bci155-3.11.json > ubuntu-table.txt
